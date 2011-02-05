620d897790be48ae95e20b715690a198be083b3b
John Scheible <johnscheible@john-scheibles-macbook-pro.local>
2011-02-04 19:53:48 -0500

    test

---
+++ src/com/example/bitmapper/startScreen.java
@@ -0,0 +1,13 @@
+package com.example.bitmapper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class startScreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
